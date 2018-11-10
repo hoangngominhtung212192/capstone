7eb6345bb91f08d5405aeab31abbf516c9a82989
TUNGHNMSE62231\Tung Hoang Ngo Minh <hoangngominhtung2121@gmail.com>
2018-11-10 18:40:19 +0700

    Update UI, sync authentication & notification

--- gwa/src/main/resources/static/js/create-model.js
+++ gwa/src/main/resources/static/js/create-model.js
@@ -32,6 +32,7 @@ $(document).ready(function () {
 
                         createdDate = jsonResponse["createdDate"].split(" ")[0];
                         getSessionProfile(jsonResponse["id"]);
+                        ajaxGetAllNotification(jsonResponse["id"]);
                     }
 
                 } else {
@@ -494,4 +495,151 @@ $(document).ready(function () {
 
         history.back(1);
     })
+
+    /*   This is for notification area   */
+    var pageNumber = 1;
+    var lastPage;
+
+    function ajaxGetAllNotification(accountID) {
+
+        $.ajax({
+            type: "GET",
+            url: "/gwa/api/notification/getAll?pageNumber=" + pageNumber + "&accountID=" + accountID,
+            success: function (result) {
+                console.log(result);
+
+                lastPage = result[0];
+                renderNotification(result[1]);
+            }
+        })
+    }
+
+    // scroll to bottom event
+    $("#ul-notification").scroll(function () {
+        if ($(this).scrollTop() + $(this).innerHeight() >= $(this)[0].scrollHeight) {
+            if (pageNumber < lastPage) {
+                pageNumber += 1;
+
+                $("#loading").css("display", "block");
+
+                setTimeout(function () {
+                    ajaxGetAllNotification(account_session_id);
+
+                    $("#loading").css("display", "none");
+                }, 400);
+            }
+        }
+    });
+
+    var countNotSeen = 0;
+
+    function renderNotification(result) {
+
+        $.each(result, function (index, value) {
+
+            var appendNotification = "";
+
+            if (!value.seen) {
+                // not seen yet
+                countNotSeen++;
+                appendNotification += "<li style='background-color: lightgoldenrodyellow;'>\n"
+            } else {
+                // already seen
+                appendNotification += "<li style='background-color: white;'>\n"
+            }
+
+            appendNotification += "<a id='" + value.id + "-" + value.notificationtype.name + "-" + value.objectID +
+                "' href=\"#\">\n" +
+                "<i class=\"fa fa-warning text-yellow\" style=\"color: darkred;\"></i> " + value.description + "</a>\n" +
+                "</li>";
+
+            $("#ul-notification").append(appendNotification);
+
+            // click event
+            $("a[id='" + value.id + "-" + value.notificationtype.name + "-" + value.objectID + "'").click(function (e) {
+                e.preventDefault();
+
+                // get parameters saved in attribute id of current notification
+                var notificationID = $(this).attr('id').split("-")[0];
+                var type = $(this).attr('id').split("-")[1];
+                var objectID = $(this).attr('id').split("-")[2];
+
+                // log to console
+                console.log("Notification ID: " + notificationID);
+                console.log("Type: " + type);
+                console.log("ObjectID: " + objectID);
+
+                // set seen status to 0 --> means user has seen this current notification
+                ajaxUpdateNotificationStatus(notificationID);
+
+                if (type == "Profile") {
+                    window.location.href = "/gwa/pages/profile.html?accountID=" + objectID;
+                } else if (type == "Model") {
+                    window.location.href = "/gwa/pages/modeldetail.html?modelID=" + objectID;
+                }
+            });
+        });
+
+        if (countNotSeen == 1 || countNotSeen == 0) {
+            $("#notice-new").text("You have " + countNotSeen + " new notification");
+        } else {
+            // plural
+            $("#notice-new").text("You have " + countNotSeen + " new notifications");
+        }
+
+        if (countNotSeen == 0) {
+            $("#numberOfNew").text("");
+        } else {
+            $("#numberOfNew").text(countNotSeen);
+        }
+
+    }
+
+    function addNewNotification() {
+        var description = $("#txtReason").val();
+
+        var formNotification = {
+            description: description,
+            objectID: account_profile_on_page_id,
+            account: {
+                id: account_profile_on_page_id
+            },
+            notificationtype: {
+                id: 1
+            }
+        }
+
+        ajaxPostNewNotification(formNotification);
+    }
+
+    function ajaxPostNewNotification(data) {
+
+        $.ajax({
+            type: "POST",
+            contentType: "application/json",
+            url: "/gwa/api/notification/addNew",
+            data: JSON.stringify(data),
+            success: function (result) {
+                console.log(result);
+            },
+            error: function (e) {
+                console.log("ERROR: ", e);
+            }
+        });
+    }
+
+    function ajaxUpdateNotificationStatus(notificationID) {
+
+        $.ajax({
+            type: "POST",
+            url: "/gwa/api/notification/update?notificationID=" + notificationID,
+            success: function (result) {
+                console.log(result);
+            },
+            error: function (e) {
+                console.log("ERROR: ", e);
+            }
+        });
+    }
+    /* End notification */
 })
